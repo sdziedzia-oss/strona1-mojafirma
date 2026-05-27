2e68a45fd1560a257dc4d3396086571fc48d498d
sdziedzia-oss <sdziedzia@gmail.com>
2026-05-27 16:41:11 +0200

    Fix Eleventy build: usun computed permalink, guard page.url
    - eleventyComputed.permalink zwracajace false bylo traktowane jako
      sciezka string 'false' w Eleventy 2.0.1 - usuniety
    - projekty.11tydata.js: tylko layout, bez computed
    - Projekty 'tylko wzmiankowane' maja normalne URL ale nie sa linkowane
      na liscie (sprawdzamy ma_podstrone !== false zamiast projekt.url)
    - head.njk + header.njk: guard 'page.url and ...' przed operator 'in'
      na falsy values (defensive fix)

--- src/projekty/projekty.11tydata.js
+++ src/projekty/projekty.11tydata.js
@@ -1,9 +1,3 @@
 module.exports = {
   layout: "projekt.njk",
-  eleventyComputed: {
-    permalink: (data) => {
-      if (data.ma_podstrone === false) return false;
-      return `/realizacje/${data.page.fileSlug}/index.html`;
-    },
-  },
 };
